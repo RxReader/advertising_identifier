c3c7a389dd918c9d42c094284b4dafde54644c90
v7lin <v7lin@qq.com>
2022-03-23 17:01:14 +0800

    github actions

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,9 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:advertising_identifier_example/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
